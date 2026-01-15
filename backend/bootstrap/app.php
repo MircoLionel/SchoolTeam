8fde138775bca67d18cfdb73e9dc1a73a1b019fb
Mirco Fermani <100171449+MircoLionel@users.noreply.github.com>
2026-01-15 15:32:27 -0300

    Configure Sanctum and CORS for API

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -12,7 +12,11 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->append(\Illuminate\Http\Middleware\HandleCors::class);
+
+        $middleware->api(prepend: [
+            \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
